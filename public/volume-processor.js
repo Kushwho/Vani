93f1cacaaa8eba39b51e6ff4a41fef6bf83f19ec
Tanmay Daga <tanmay06daga@gmail.com>
2024-09-12 01:19:56 +0530

    testcpmmmu

--- public/volume-processor.js
+++ public/volume-processor.js
@@ -7,7 +7,7 @@ class AudioLevelProcessor extends AudioWorkletProcessor {
 
   }
 
-  process(inputs, outputs) {
+  process(inputs, outputs, parameters) {
     const input = inputs[0];
     const output = outputs[0];
     console.log("I am loaded")
