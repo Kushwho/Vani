3b4adeda0537bb165181beaa96861afcbcfef05e
Tanmay Daga <tanmay06daga@gmail.com>
2024-09-14 00:20:05 +0530

    fixedLogging

--- src/util/StringHandler.ts
+++ src/util/StringHandler.ts
@@ -1,7 +1,11 @@
 export function replaceSpacesWithHyphens(inputString:string) {
+  console.log(inputString);
+  
   return inputString.replace(/ /g, "-");
 }
 
 export function replaceHyphensWithSpaces(inputString:string) {
+  console.log(inputString);
+  
   return inputString.replace(/-/g, " ");
 }
